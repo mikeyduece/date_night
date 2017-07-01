8352990160eb4d5fe8ba1959bdc881771147f339
Mike Heft <mikeheft@gmail.com>
2017-07-01 15:21:16 -0600

    insert done and understood 4x

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -4,29 +4,30 @@ class BinarySearchTree
   attr_reader :root
 
   def initialize(root=nil)
-    @root = root
+    @root  = root
+    @level = 0
   end
 
   def insert(score, title, level=0, current_node=@root)
     @level = level
     if @root.nil?
-      @root = Node.new(score, title, @level)
+      @root = Node.new(score, title, @level, current_node)
       @root.depth
     else
       @level += 1
       if score < current_node.score
         if current_node.left_node.nil?
           current_node.left_node = Node.new(score, title, @level, current_node)
-          current_node.left_node.depth
+          current_node.depth
         else
           insert(score, title, @level, current_node.left_node)
         end
       elsif score > current_node.score
         if current_node.right_node.nil?
           current_node.right_node = Node.new(score, title, @level, current_node)
-          current_node.right_node.depth
+          current_node.depth
         else
-          insert(score, title, @level, current_node.right_node).depth
+          insert(score, title, @level, current_node)
         end
       end
     end
